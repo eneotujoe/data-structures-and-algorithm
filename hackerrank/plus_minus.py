068f0255c94277a3c491ccde3456ccc8029cda97
eneotujoe <eneotujoe@gmail.com>
2022-04-19 12:04:29 +0100

    added more solutions

--- hackerrank/plus_minus.py
+++ hackerrank/plus_minus.py
@@ -5,7 +5,6 @@ def plus_minus(arr):
     print(f'{positive_count/len(arr):.6f}\n{negative_count/len(arr):.6f}\n{zero_count/len(arr):.6f}')
     
 
-
 if __name__ == '__main__':
 
     arr = [-4, 3, -9, 0, 4, 1]
